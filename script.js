747bae2c82faf23b0b9e4051ac28f2a20d0be806
Nizami Sevindi <nsevindi87@gmail.com>
2022-08-19 01:32:46 +0200

    Javascript is been written

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+//SIDEBAR
 const menuItems = document.querySelectorAll('.menu-item');
 
+//MESSAGES
+const messagesNotification = document.querySelector('#messages-notification');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search')
+
+
+//remove active class from all menu items
 const changeActiveItem = () =>{
     menuItems.forEach(item => {
         item.classList.remove('active')
@@ -18,4 +27,38 @@ menuItems.forEach(item=>{
             document.querySelector('#notification .notification-count').style.display = 'none'
         }
     })
+})
+
+
+/* -------------MESSAGES----------- */
+//searches chat
+const searchMessage = () =>{
+    const va1 = messageSearch.value.toLowerCase()
+    message.forEach(chat=>{
+        let name = chat.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(va1) != -1){
+            chat.style.display = 'flex';
+        }else{
+            chat.style.display ='none';
+        }
+    })
+}
+
+
+//search chat
+messageSearch.addEventListener('keyup', searchMessage)
+
+
+
+
+
+
+
+//highleight messages card when messages menu item is clicked
+messagesNotification.addEventListener('click', ()=>{
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display ='none'
+    setTimeout(() => {
+        messages.style.boxShadow ='none'
+    }, 2000);
 })
